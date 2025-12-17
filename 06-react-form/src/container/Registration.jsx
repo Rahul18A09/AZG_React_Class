329562172dd0eb3b36b4709640dcac40ef095d33
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-17 11:51:52 +0530

    update

--- 06-react-form/src/container/Registration.jsx
+++ 06-react-form/src/container/Registration.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Registration = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passwordRef.current.value);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="fullname">Full Name:</label>
+          <input type="text" name="fullname" id="fullname" ref={nameRef} />
+        </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <input type="email" name="email" id="email" ref={emailRef} />
         </div>
 
         <div>
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" name="password" id="password"ref={passwordRef} />
         </div>
 
-        <Button onSubmit={handleSubmit} />
+        <button type="submit">Register</button>
+
       </form>
     </div>
   );
