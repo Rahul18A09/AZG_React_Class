57b1191c709ddc45472ac5a08afea7e565bbd311
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-17 11:12:19 +0530

    update

--- 06-react-form/src/container/Registration.jsx
+++ 06-react-form/src/container/Registration.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Registration = () => {
   return (
     <div>
-      <h1>Registration</h1>
-    </div>
-  )
-}
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-export default Registration
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <Button onSubmit={handleSubmit} />
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
