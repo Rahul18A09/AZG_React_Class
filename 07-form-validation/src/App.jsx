e0e460791ff7bcc63075397bab8f2253624d601e
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-19 23:43:28 +0530

    update

--- 07-form-validation/src/App.jsx
+++ 07-form-validation/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from './container/LoginForm'
 
 const App = () => {
   return (
     <div>
-      
+      <LoginForm/>
     </div>
   )
 }
--- 07-form-validation/src/container/LoginForm.jsx
+++ 07-form-validation/src/container/LoginForm.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      fullname: "",
+      email: "",
+      password: "",
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(6, "Name must be at least 6 characters")
+        .required("Name is required"),
+
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+
+      // password: Yup.string()
+      //   .min(6, "Password must be at least 6 characters")
+      //   .required("Password is required"),
+    }),
+
+    onSubmit: (values) => {
+      console.log("Form Data:", values);
+    },
+  });
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Login Form</h2>
+        <div>
+          <label htmlFor="fullname">Full Name:</label>
+          <input
+            type="text"
+            id="fullname"
+            name="fullname"
+            value={formik.values.fullname}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.fullname && formik.errors.fullname && (
+            <p style={{ color: "red" }}>{formik.errors.fullname}</p>
+          )}
+        </div>
 
-export default LoginForm
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <p style={{ color: "red" }}>{formik.errors.email}</p>
+          )}
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
