81727cced2e800a1d1552160cafc398f14c30448
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-20 16:50:33 +0530

    update

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Todo from './component/Todo'
 
 const App = () => {
   return (
     <div>
-      
+      <Todo/>
     </div>
   )
 }
--- todo-app/src/component/Todo.jsx
+++ todo-app/src/component/Todo.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { useState } from "react";
 
-const Todo = () => {
+function Todo() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo) {
+      setTodos([...todos, { text: newTodo, completed: false }]);
+      setNewTodo("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <h1>Todo</h1>
-    </div>
-  )
-    }
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Add new todo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+        </div>
 
-export default Todo
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none"
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
