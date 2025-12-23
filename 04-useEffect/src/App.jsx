fa0b1f67725ec5d9472bebdc462a749060fa93da
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-23 18:11:40 +0530

    update

--- 04-useEffect/src/App.jsx
+++ 04-useEffect/src/App.jsx
@@ -1,36 +1,10 @@
-import Effects from "./components/Effects"
-import Users1 from "./components/Users1"
-import Counter from "./Counter"
-import First from "./First"
-import InputExample from "./InputExample"
-import LikedButton from "./LikedButton"
-import Student from "./Student"
-import Timer from "./Timer"
-import ToggleText from "./ToggleText"
-import UserProfile from "./UserProfile"
-import Users from "./Users"
-import WindowsSizeTracker from "./WindowsSizeTracker"
+import First from "./components/First";
 
-
-function App(){
-
-
-return (
-  <>
-  {/* <First/> */}
-  {/* <Timer/> */}
-  {/* <WindowsSizeTracker/> */}
-  {/* <Users/> */}
-  {/* <Counter/> */}
-{/* <Effects/> */}
-{/* <Users1/> */}
-{/* <ToggleText/> */}
-{/* <LikedButton/> */}
-{/* <UserProfile/> */}
-{/* <Student/> */}
-<InputExample/>
-  </>
-)
-
+function App() {
+  return (
+    <>
+      <First />
+    </>
+  );
 }
-export default App
+export default App;
---
+++ 04-useEffect/src/components/First.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+function First() {
+
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+        console.log("Component Re-rendered!");
+        
+    },[count]);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default First
