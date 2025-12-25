f81b73f8b381a5ecd83d4e315e45c39d89f74476
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-25 23:26:52 +0530

    update

--- react-simple-crud/src/Crud.jsx
+++ react-simple-crud/src/Crud.jsx
@@ -17,22 +17,43 @@ function Crud() {
     },
 
     validationSchema,
-    onSubmit: (user) => {
+    onSubmit: (user, {resetForm}) => {  
+      user.id = Date.now();
       setUsers([...users, user]);
+      resetForm();
     },
   });
+
+  function deleteUser (id){
+    let  newUsersData = users.filter((user) => user.id !== id)
+    setUsers(newUsersData);
+  }
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={formik.values.name} onChange={formik.handleChange} />
+          <label htmlFor="name">Name:</label>
+        ,  <input
+            type="text"
+            id="name"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+          />
+          <span className="text-danger">{formik.errors.name}</span>
         </div>
 
         <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={formik.values.email} onChange={formik.handleChange} />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          <span className="text-danger">{formik.errors.email}</span>
         </div>
 
         <button type="submit">Save</button>
@@ -40,19 +61,38 @@ function Crud() {
 
       <div>
         <table>
-            <thead>
-                <tr>
-                    <td>#</td>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Action</td>
-                </tr>
-            </thead>
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Action</td>
+            </tr>
+          </thead>
 
-            <tbody>
-                {users &&  Array.isArray(users)
-                }
-            </tbody>
+          <tbody>
+            {users && Array.isArray(users) && users.length > 0 ? (
+              users.map((user, index) => (
+                <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button type="button">Update</button>
+                    <button type="button" onClick={() => deleteUser(user.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td>
+                  <p>Data not Found</p>
+                </td>
+              </tr>
+            )}
+          </tbody>
         </table>
       </div>
     </div>
