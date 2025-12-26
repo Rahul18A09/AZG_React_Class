820060df438e5aab25e4ba84ec1aca530e3e6a6c
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-26 22:39:40 +0530

    update

--- react-simple-crud/src/App.jsx
+++ react-simple-crud/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Users from "./Users";
 import Crud from "./Crud";
+import UserForm from "./UserForm";
 
 function App() {
   return (
     <div>
       {/* <Users /> */}
       {/* <Crud /> */}
+      <UserForm/>
     </div>
   );
 }
--- react-simple-crud/src/UserForm.jsx
+++ react-simple-crud/src/UserForm.jsx
@@ -1,43 +1,109 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Card, CardBody, Container, Form ,CardTitle} from "reactstrap";
+import {
+  CardBody,
+  CardTitle,
+  Card,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  FormFeedback,
+  Button,
+  Col,
+  Row,
+} from "reactstrap";
 
 const UserForm = () => {
   const formik = useFormik({
-     initialValues: {
+    initialValues: {
       name: "",
       email: "",
-      password: "",
+      //   password: "",
     },
-  })
 
     validationSchema: Yup.object({
       name: Yup.string().required("Name is required!"),
       email: Yup.string().email().required("Email is required!"),
-      password: Yup.string().min(6, "Password must be at least 6 characters!"),
+      //   password: Yup.string().required("Password is required!"),
     }),
 
     onSubmit: (values, { resetForm }) => {
-      localStorage.setItem("userData", JSON.stringify(values));
-      alert("USer Data Saved in localStorage Successfully!");
+      localStorage.setItem("usersData", JSON.stringify(values));
+      alert("User data saved in localstorage");
       resetForm();
     },
-  };
+  });
 
   return (
-    <Container>
-        <Card>
+    <Container className="mt-5">
+      <Row>
+        <Col sm-4>
+          <Card className="shadow">
             <CardBody>
-                <CardTitle tag="h4">
-User Registration
-                </CardTitle>
+              <CardTitle tag="h4" className="mb-4">
+                User Registration
+              </CardTitle>
+              <Form onSubmit={formik.handleSubmit}>
+                <FormGroup>
+                  <Label for="name">Name:</Label>
+                  <Input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter Your name"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    values={formik.values.name}
+                    invalid={formik.touched.name && Boolean(formik.errors.name)}
+                  />
+                  <FormFeedback>{formik.errors.name}</FormFeedback>
+                </FormGroup>
 
-                <Form>
+                <FormGroup>
+                  <Label for="name">Email:</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Your Email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    values={formik.values.email}
+                    invalid={
+                      formik.touched.email && Boolean(formik.errors.email)
+                    }
+                  />
+                  <FormFeedback>{formik.errors.email}</FormFeedback>
+                </FormGroup>
 
-                </Form>
+                {/* <FormGroup>
+              <Label for="name">Password:</Label>
+              <Input
+                type="password"
+                id="Password"
+                name="Password"
+                placeholder="Enter Your Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                values={formik.values.password}
+                invalid={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+              />
+              <FormFeedback>{formik.errors.password}</FormFeedback>
+            </FormGroup> */}
+
+                <Button color="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
             </CardBody>
-        </Card>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
