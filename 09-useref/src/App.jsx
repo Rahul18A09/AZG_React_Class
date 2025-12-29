2283c8bd8f133a07caf69fccec799f34f65b0595
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-29 11:46:08 +0530

    new

---
+++ 09-useref/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useState } from "react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // function handleClick() {
+  //   setCount((count) => count + 1);
+  // }
+
+  // const inputRef = useRef(null);
+
+  const countRef = useRef(0);
+
+
+
+  // const handleFocus = () => {
+  //   inputRef.current.focus();
+  // }
+
+  return (
+    <>
+      {/* <div>
+        <h2>Count: {count}</h2>
+        <button onClick={handleClick}>Increament</button>
+      </div> */}
+
+{/* <div>
+<input type="text" placeholder="Click button to focus" ref={inputRef} />
+<button onClick={handleFocus}>Focus Input</button>
+</div> */}
+
+<div>
+  
+</div>
+
+    </>
+  );
+}
+
+export default App;
