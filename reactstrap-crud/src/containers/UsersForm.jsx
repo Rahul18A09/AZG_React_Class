1d1ecc7fc1e206d8c3244ce6ecb47aa068e3e6cb
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-01 17:25:38 +0530

    new

---
+++ reactstrap-crud/src/containers/UsersForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UsersForm(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UsersForm;
---
+++ reactstrap-crud/src/containers/UsersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UsersList(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UsersList;
