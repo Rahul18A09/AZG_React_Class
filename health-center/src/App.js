7328347ddd7b52e93261adc037722c1d7d84239b
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-05 19:32:52 +0530

    new

--- health-center/src/App.js
+++ health-center/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 
 // Public pages
 import Home from "./container/Home";
@@ -12,37 +12,12 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 
-// Admin
-import AdminLogin from "./admin/auth/AdminLogin";
-import AdminLayout from "./admin/layout/AdminLayout";
-import ProtectedRoute from "./admin/auth/ProtectedRoute";
-
-// Admin pages
-import Dashboard from "./admin/pages/Dashboard";
-import AdminDoctors from "./admin/pages/AdminDoctors";
-
-
 function App(props) {
   return (
     <div>
       <Header />
       <Routes>
-        {/* PUBLIC WEBSITE */}
         <Route path="/" element={<Home />} />
-
-        {/* 🔐 ADMIN LOGIN */}
-        <Route path="/admin/login" element={<AdminLogin />} />
-
-        {/* Admin panel (Protected) */}
-        <Route path="/admin" element={<ProtectedRoute>
-          <AdminLayout/>
-        </ProtectedRoute>} />
-
-         {/* Default admin route */}
-         <Route index element={<Navigate to="dashboard"/>}/>
-         <Route path="dashboard" element={<Dashboard/>}/>
-         <Route path="doctors" element={<AdminDoctors/>}/>
-
         <Route path="/about" element={<AboutUs />} />
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/news" element={<News />} />
--- health-center/src/index.js
+++ health-center/src/index.js
@@ -4,16 +4,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "font-awesome/css/font-awesome.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { Provider } from "react-redux";
-import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
