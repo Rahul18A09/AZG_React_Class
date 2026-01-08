cc8fe728321a6a61e5ee6619564e6e40ba6a7fa0
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-08 19:01:18 +0530

    new

--- health-center/src/App.js
+++ health-center/src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
+
 import UserRoutes from "./routes/UserRoutes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SideNav from "./container/admin/SideNav";
+
+
+
+
+import Doctors from "./container/admin/pages/Doctors";
+import Appointment from "./container/admin/pages/Appointment";
+import ContactUs from "./container/admin/pages/ContactUs";
+import Dashboard from "./container/admin/pages/Dashboard";
+
 
 function App(props) {
   return (
     <>
-      <Header />
+      {/* <Header />
       <UserRoutes />
-      <Footer />
+      <Footer /> */}
+
+      {/* <SideNav/> */}
+      <Routes>
+        <Route path="/" exact element={<Dashboard/>}/>
+        <Route path="/doctors" element={<Doctors/>}/>
+        <Route path="/appointment" element={<Appointment/>}/>
+        <Route path="/contact" element={<ContactUs/>}/>
+      </Routes>
     </>
   );
 }
---
+++ health-center/src/container/admin/pages/Appointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Appointment(props) {
+    return (
+        <div>
+            Appointment
+        </div>
+    );
+}
+
+export default Appointment;
