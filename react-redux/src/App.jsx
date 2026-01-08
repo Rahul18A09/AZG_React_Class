183be80b37e6d1d7605655dd5a19b344556c8a72
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-08 22:28:17 +0530

    update

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,21 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
-import { decrement, increment, reset} from "./redux/CounterSlice";
+import { decrement, increment, reset } from "./redux/CounterSlice";
+import Navbar from "./Navbar";
 
 function App() {
-
-const count = useSelector((state) => state.counter.value);
-const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
+
     <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Count: {count}</h1>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
-      </div>
+    <Navbar/>
+     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
     </>
+   
   );
 }
 
