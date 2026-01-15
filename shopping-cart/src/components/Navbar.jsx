b6f077050d16b91861330d5dff9b1a1f19b1108d
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-15 16:40:41 +0530

    update

---
+++ shopping-cart/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand fw-bold" to="/">
+          🛒 MyShop
+        </Link>
+        <div>
+          <Link className="nav-link d-inline me-3 text-white" to="/">
+            Home
+          </Link>
+          <Link className="nav-link d-inline text-white" to="/cart">
+            Cart (2)
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
