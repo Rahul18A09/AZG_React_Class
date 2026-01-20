be16d29acf38a5acb8ae02fc91b1f33bbdd098a7
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-20 11:42:05 +0530

    practice

---
+++ react-revision/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Rahul</h1>
+      <h2>FrontEnd Developer</h2>
+    </div>
+
+  )
+}
+
+export default App
---
+++ react-revision/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Rahul</h1>
+      <h2>FrontEnd Developer</h2>
+    </div>
+  );
+}
+
+export default Profile;
