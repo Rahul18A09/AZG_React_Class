a7d4219ade51eb99d1f9a3bbbb497998388c10df
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-21 13:14:22 +0530

    update

--- react-router/src/Products.jsx
+++ react-router/src/Products.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function Products() {
   return (
     <div>
       <h1>Products Page</h1>
       <nav>
-        <Link to="/phone">Phone</Link> |
-        <Link to="/laptop">Laptop</Link>
+        <Link to="phone">Phone</Link> |<Link to="laptop">Laptop</Link>
       </nav>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
