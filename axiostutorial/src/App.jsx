ea29fcb49f323874909c9bb2d5d7c31ffa1c47b6
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-26 19:31:39 +0530

    new

---
+++ axiostutorial/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Users from './Users'
+
+function App() {
+ 
+  return (
+    <>
+    <Users/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ axiostutorial/src/Users.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User Data</h1>
+      {users.map((user) => (
+        <div>
+          <h2 key={user.id}>{user.name}</h2>
+          <p>{user.username}</p>
+          <p>{user.email}</p>
+
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
