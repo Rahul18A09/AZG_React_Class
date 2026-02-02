14e548488cb5af179bc48c008dca9d1f8bf66788
Rahul18A09 <rahulbharda60@gmail.com>
2026-02-02 11:21:45 +0530

    update

--- react-hook-form/src/Form.jsx
+++ react-hook-form/src/Form.jsx
@@ -2,7 +2,28 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 function Form() {
-  const { register, handleSubmit, control } = useForm();
+  //   const { register, handleSubmit, control, formState } = useForm({
+  //     defaultValues:{
+  //         name:"Rahul",
+  //         email:"example@gmail.com",
+  //         age:"18"
+  //     }
+  //   });
+
+  const { register, handleSubmit, control, formState } = useForm({
+    defaultValues: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+
+      return {
+        name: data.name,
+        email: data.email,
+        age: 18,
+      };
+    },
+  });
+
+  const { errors } = formState;
 
   console.log(useForm());
 
@@ -20,6 +41,7 @@ function Form() {
           id="name"
           {...register("name", { required: "Name is required" })}
         />
+        {errors.name && <p className="error">{errors.name?.message}</p>}
         <br />
 
         <label htmlFor="email">Email:</label>
@@ -33,8 +55,25 @@ function Form() {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
               message: "Invalid email address",
             },
+
+            validate: {
+              notAdminEmail: (value) => {
+                return (
+                  value !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
+              },
+
+              notBlacklistedEmail: (value) => {
+                return (
+                  !value.endsWith("@blacklisted.com") ||
+                  "This domain is not allowed."
+                );
+              },
+            },
           })}
         />
+        {errors.email && <p className="error">{errors.email?.message}</p>}
         <br />
 
         <label htmlFor="age">Age:</label>
@@ -46,9 +85,10 @@ function Form() {
             valueAsNumber: true,
             required: "Age is required",
             min: { value: 18, message: "Age must be at least 18" },
-            max: { value: 25, message: "Age must be at most" },
+            max: { value: 25, message: "Age must be at most 25" },
           })}
         />
+        {errors.age && <p className="error">{errors.age?.message}</p>}
         <br />
         <button type="submit">Submit</button>
       </form>
