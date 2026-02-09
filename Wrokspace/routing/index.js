75c691e3b3be7b5a02caec21fdfdce4d69aa7692
Rahul18A09 <rahulbharda60@gmail.com>
2026-02-09 20:50:25 +0530

    update

--- Wrokspace/routing/index.js
+++ Wrokspace/routing/index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// Request ki kahani
+
+app.get("/", (req, res) => {
+    res.send("Got a  GET Request")
+});
+
+app.post("/items", (req,res) => {
+    res.send("Got a POST Request")
+});
+
+app.put("/items/:id", (req, res) => {
+    res.send("Got a PUT Request")
+});
+
+app.delete("/items/:id", (req,res) => {
+    res.send("Got a DELETE Request")
+}); 
 
 app.listen(port, () => {
   console.log(`Rahul app listening on port ${port}`)
